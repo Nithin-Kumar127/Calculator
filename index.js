663b61e09d2212e10d51affcce26313e9f7cf2db
Nithin-Kumar127 <nithinkumarj.mail@gmail.com>
2025-06-04 23:12:58 +0530

    First Upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const display = document.getElementById("display");
+let input = '';
+
+function addNumber(num){
+    input += num;
+    display.value = input;
+}
+
+function addOperator(op){
+    if (input === '' && op !== '-') {
+       return ;
+    }
+
+    const lastChar = input[input.length - 1];
+            if ('+-*/%'.includes(lastChar)) {
+                input = input.slice(0, -1) + op; // Replace last operator
+            } 
+            else {
+                input += op;
+            }
+            display.value = input;
+}
+
+const parts = input.split(/[+\-*/%]/);
+            const currentNum = parts[parts.length - 1];
+             if (!currentNum.includes('.')) {
+                input += '.';
+                display.value = input;
+            }
+ 
+         function toggleSign() {
+            if (!input) return;
+            
+            if (input.startsWith('-')) {
+                input = input.substring(1);
+            } else {
+                input = '-' + input;
+            }
+            display.value = input;
+        }           
+
+function clearDisplay(){
+    input = "";
+    display.value = input ;
+  }
+
+  function calculate(){
+    display.value = eval(display.value);
+  }
+
+
+
+
