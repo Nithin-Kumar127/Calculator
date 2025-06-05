734c47ad93ebd96acc74cd296afe27d30fbb02ed
Nithin-Kumar127 <nithinkumarj.mail@gmail.com>
2025-06-05 08:57:16 +0530

    last Upload

--- index.js
+++ index.js
@@ -1,44 +1,64 @@
 const display = document.getElementById("display");
-let input = '';
+        let input = '';
 
-function addNumber(num){
-    input += num;
-    display.value = input;
-}
+        function addNumber(num) {
+            input += num;
+            display.value = input;
+        }
 
-function addOperator(op){
-    if (input === '' && op !== '-') {
-       return ;
-    }
+        function addOperator(op) {
+            if (input === '' && op !== '-') {
+                return;
+            }
 
-    const lastChar = input[input.length - 1];
+            const lastChar = input[input.length - 1];
             if ('+-*/%'.includes(lastChar)) {
-                input = input.slice(0, -1) + op; // Replace last operator
-            } 
-            else {
+                input = input.slice(0, -1) + op; 
+            } else {
                 input += op;
             }
             display.value = input;
-}
+        }
 
-const parts = input.split(/[+\-*/%]/);
+        function addDecimal() {
+          
+            const parts = input.split(/[+\-*/%]/);
             const currentNum = parts[parts.length - 1];
-             if (!currentNum.includes('.')) {
+            
+          
+            if (!currentNum.includes('.')) {
                 input += '.';
                 display.value = input;
             }
-        
+        }
 
-function clearDisplay(){
-    input = "";
-    display.value = input ;
-  }
+        function toggleSign() {
+            if (!input) return;
+            
+            if (input.startsWith('-')) {
+                input = input.substring(1);
+            } else {
+                input = '-' + input;
+            }
+            display.value = input;
+        }
 
-  function calculate(){
-    display.value = eval(display.value);
-  }
+        function clearDisplay() {
+            input = "";
+            display.value = "";
+        }
 
-  display.value = "";
+        function calculate() {
+            try {
+              
+                let expression = input.replace(/%/g, '/100');
+                let result = eval(expression);
+                input = result.toString();
+                display.value = input;
+            } catch (error) {
+                display.value = "Error";
+                input = "";
+            }
+        }
 
-
-
+      display.value = "";
